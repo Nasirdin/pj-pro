c48c88c5569b817c7c82416d8af785c2383a51b8
Nasirdin <nasirdin.zhaparkulov@gmail.com>
2022-11-19 18:52:43 +0600

    update

--- index..js
+++ index..js
@@ -395,7 +395,7 @@ bot.action(`clock`, async (ctx) => {
 // CRON ===============================================
 let textOfTheDay = 15;
 
-cron.schedule("0 6 * * *", async () => {
+cron.schedule("0 19 * * *", async () => {
   const timeOut = allUsers.map((element) => {
     const newBonus = {
       userId: element.userId,
